2295ddb383b247289b50e07316c4f80b9c8cd946
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-10-30 20:30:07 -0500

    first commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace VariablesExercise
+﻿using System.Diagnostics;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string catName = "Brian";
+            int catLives = 13;
+            char catBay = 'A';
+            bool catGood = false;
+            double catDeaths = 2.3;
+            decimal catAverage = 14.23M;
+
+            Console.WriteLine($"I once new a boy who had a cat named {catName}\n" +
+                $"A cat in a hat he was not, for {catLives} did {catName} have\n" +
+                $"At apartment {catBay} {catName} did stay\n" +
+                $"Where any good intentions were vehemently {catGood} \n" +
+                $"For {catName} was a bad cat for sure, {catDeaths} dead squirels per week \n" +
+                $"With a penchant for the {catAverage} did favor...");
         }
     }
 }
